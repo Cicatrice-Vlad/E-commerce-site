0abdb6c773a0c6912be47fa774ae8d11e5f1150d
webdevtonym <75320313+webdevtonym@users.noreply.github.com>
2023-02-08 20:02:33 +0000

    added cards

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,25 @@
 
-var cardArr = [0, 1, 2, 3, 4, 5,];
+// var cardArr = [0, 1, 2, 3, 4, 5,];
 
-function displayBlock() {
-  var blockHTML = "";
-  var currentDiv = cardArr[i];
+// function displayBlock() {
+//   var blockHTML = "";
+//   var currentDiv = cardArr[i];
 
-  for (var i = 0; i < cardArr.length; i++) {
-    blockHTML += `<div class="card mb-4 ml-auto">
-        <img class="card-img-top" src="..." alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-          </p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-    </div>`;
-  }
+//   for (var i = 0; i < cardArr.length; i++) {
+//     blockHTML += `<div class="card mb-4 ml-auto">
+//         <img class="card-img-top" src="..." alt="Card image cap" />
+//         <div class="card-body">
+//           <h5 class="card-title">Card title</h5>
+//           <p class="card-text">
+//           </p>
+//         </div>
+//         <div class="card-footer">
+//           <small class="text-muted">Last updated 3 mins ago</small>
+//         </div>
+//     </div>`;
+//   }
 
-  $("#cards-div").html(blockHTML);
-}
+//   $("#cards-div").html(blockHTML);
+// }
 
-displayBlock();
+// displayBlock();
