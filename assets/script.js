87828f230fc66b42067cc9baef5d8714b7f277db
webdevtonym <75320313+webdevtonym@users.noreply.github.com>
2023-02-09 18:14:09 +0000

    added google maps API to web page

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,11 @@
-var cardArr = [0, 1, 2, 3, 4, 5];
-
-// function displayBlock() {
-//   var blockHTML = "";
-//   var currentDiv = cardArr[i];
-
-  for (var i = 0; i < cardArr.length; i++) {
-    blockHTML += `<div class="card mb-4 ml-auto">
-        <img class="card-img-top" src="..." alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-          </p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-    </div>`;
-  }
-
-//   $("#cards-div").html(blockHTML);
-// }
-
-displayBlock();
+function initMap(){
+  var location = { lat: 30.04442, lng: 31.235712 };
+  var map = new google.maps.Map(document.getElementById("map"),{
+    zoom: 4, 
+    center: location
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
